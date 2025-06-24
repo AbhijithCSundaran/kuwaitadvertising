7e4dfb875ca747cb5dbe9e950aa59589c83d8908
sandra <sandraachu2211@gmail.com>
2025-06-24 14:58:19 +0530

    list update

--- app/Views/companylist.php
+++ app/Views/companylist.php
@@ -65,6 +65,23 @@
     </table>
 </div>
 </div>
+<div class="modal fade" id="confirmDeleteModal" tabindex="-1" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="confirmDeleteModalLabel">Confirm Deletion</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Are You Sure You Want To Delete This Company?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+        <button type="button" id="confirm-delete-btn" class="btn btn-danger">Delete</button>
+      </div>
+    </div>
+  </div>
+</div>
 <?php include "common/footer.php"; ?>   
 <script>
     let table="";
