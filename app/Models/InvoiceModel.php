704e25664fe160d10a8ad1b1c5e8ecd469ba3920
shafeena <shafeenaki123@gmail.com>
2025-08-07 15:45:17 +0530

    invoice,invoice model,adduser

--- app/Models/InvoiceModel.php
+++ app/Models/InvoiceModel.php
@@ -25,9 +25,9 @@ class InvoiceModel extends Model
     'company_id' ];
     protected $returnType = 'array';
 
-     public function getInvoiceCount()
+     public function getInvoiceCount($companyId)
     {
-        return $this->db->table('invoices')->countAllResults();
+          return $this->where('company_id', $companyId)->countAllResults();
     }
     public function getInvoiceListWithCustomer()
 {
@@ -36,23 +36,42 @@ class InvoiceModel extends Model
                 ->orderBy('invoices.invoice_id', 'desc')
                 ->findAll();
 }
-   public function getFilteredCount($search = '')
+//    public function getFilteredCount($search = '')
+//     {
+//         $builder = $this->db->table('invoices')
+//             ->join('customers', 'customers.customer_id = invoices.customer_id', 'left');
+
+//         if ($search) {
+//             $search = trim(strtolower($search));
+
+//             $builder->groupStart()
+//                 ->like('LOWER(customers.name)', $search)
+//                 ->orLike('LOWER(customers.address)', $search)
+//                 ->orLike('FORMAT(invoices.discount, 2)', $search)
+//                 ->orLike('DATE_FORMAT(invoices.invoice_date, "%d-%m-%Y")', $search)
+//                 ->groupEnd();
+//         }
+//         return $builder->countAllResults();
+//     }
+    public function getFilteredCount($searchValue, $companyId)
     {
         $builder = $this->db->table('invoices')
-            ->join('customers', 'customers.customer_id = invoices.customer_id', 'left');
+            ->join('customers', 'customers.customer_id = invoices.customer_id', 'left')
+            ->where('invoices.company_id', $companyId);
 
-        if ($search) {
-            $search = trim(strtolower($search));
-
+        if (!empty($searchValue)) {
+            $searchValue = trim(strtolower($searchValue));
             $builder->groupStart()
-                ->like('LOWER(customers.name)', $search)
-                ->orLike('LOWER(customers.address)', $search)
-                ->orLike('FORMAT(invoices.discount, 2)', $search)
-                ->orLike('DATE_FORMAT(invoices.invoice_date, "%d-%m-%Y")', $search)
+                ->like('LOWER(customers.name)', $searchValue)
+                ->orLike('LOWER(customers.address)', $searchValue)
+                ->orLike('FORMAT(invoices.discount, 2)', $searchValue)
+                ->orLike('DATE_FORMAT(invoices.invoice_date,"%d-%m-%Y")', $searchValue)
                 ->groupEnd();
         }
+
         return $builder->countAllResults();
     }
+
 
 
 
@@ -61,11 +80,12 @@ class InvoiceModel extends Model
     $builder = $this->db->table('invoices')
         ->select('invoices.invoice_id, invoices.customer_id, invoices.discount, invoices.total_amount, invoices.invoice_date, invoices.shipping_address, invoices.phone_number, invoices.lpo_no, invoices.status, customers.name AS customer_name, customers.address AS customer_address')
         ->join('customers', 'customers.customer_id = invoices.customer_id', 'left')
-        ->join('user', 'user.user_id = invoices.user_id', 'left');
+        ->join('user', 'user.user_id = invoices.user_id', 'left')
+        ->where('invoices.company_id', $companyId); 
 
-    if ($companyId) {
-        $builder->where('user.company_id', $companyId);
-    }
+    // if ($companyId) {
+    //     $builder->where('user.company_id', $companyId);
+    // }
     if ($search) {
         $search = trim(strtolower($search));
         $builder->groupStart()
